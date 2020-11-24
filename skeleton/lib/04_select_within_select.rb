61887dbe44641f75840307ae59ef199f996cd5f7
Jose Miralles <josemiralles1991@gmail.com>
2020-11-24 15:49:20 -0500

    break time

--- skeleton/lib/04_select_within_select.rb
+++ skeleton/lib/04_select_within_select.rb
@@ -35,7 +35,15 @@ end
 def larger_than_russia
   # List each country name where the population is larger than 'Russia'.
   execute(<<-SQL)
-  
+    SELECT
+    name
+    FROM countries
+    WHERE
+      Population > (
+        SELECT population
+        FROM countries
+        WHERE name LIKE 'Russia'
+      )
   SQL
 end
 
@@ -43,6 +51,14 @@ def richer_than_england
   # Show the countries in Europe with a per capita GDP greater than
   # 'United Kingdom'.
   execute(<<-SQL)
+    SELECT name
+    FROM Countries
+    WHERE continent LIKE 'Europe'
+    AND gdp/population > (
+      SELECT gdp/population
+      FROM countries
+      WHERE name LIKE 'United Kingdom'
+    )
   SQL
 end
 
@@ -50,6 +66,13 @@ def neighbors_of_certain_b_countries
   # List the name and continent of countries in the continents containing
   # 'Belize', 'Belgium'.
   execute(<<-SQL)
+    SELECT name, continent
+    FROM countries
+    WHERE continent IN (
+      SELECT continent
+      FROM countries
+      WHERE name IN ('Belize', 'Belgium')
+    )
   SQL
 end
 
@@ -57,6 +80,18 @@ def population_constraint
   # Which country has a population that is more than Canada but less than
   # Poland? Show the name and the population.
   execute(<<-SQL)
+    SELECT name, population
+    FROM countries
+    WHERE population > (
+      SELECT population
+      FROM countries
+      WHERE name IN ('Canada')
+    )
+    AND population < (
+      SELECT population
+      FROM countries
+      WHERE name IN ('Poland')
+    )
   SQL
 end
 
